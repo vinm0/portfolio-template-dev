6fa2f2da0a15fdce40a57dd8762edf6941ef6694
Vincent E Martinez <vincentem20@gmail.com>
2025-08-01 10:41:40 -0500

    update dependencies

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     ppr: 'incremental',
+  },
+  output: 'standalone',
+  images: {
+    domains: ['localhost'],
+    unoptimized: process.env.NODE_ENV === 'development'
   }
 };
 
